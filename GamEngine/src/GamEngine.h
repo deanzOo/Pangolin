fb468e7fe16edc6bd177fbc7218f4ef1c7d95a59
dean <deanabutbul@gmail.com>
2022-11-02 23:18:02 +0200

    internal keycodes and mouse button codes

--- GamEngine/src/GamEngine.h
+++ GamEngine/src/GamEngine.h
@@ -1,8 +1,15 @@
 #pragma once
 
 #include "GamEngine/App.h"
+
+#include "GamEngine/Input.h"
+
+#include "GamEngine/KeyCodes.h"
+#include "GamEngine/MouseButtonCodes.h"
+
 #include "GamEngine/Layer.h"
-#include "GamEngine/Log.h"
 #include "GamEngine/ImGui/ImGuiLayer.h"
+
+#include "GamEngine/Log.h"
 
 #include "GamEngine/Entry.h"
